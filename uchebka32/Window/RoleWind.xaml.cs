e35215ea9b9ffabeecbb527c4ec543a7fda12878
Yulia Karelova <j.karelowa@gmail.com>
2025-04-01 17:26:05 +0300

    second

---
+++ uchebka32/Window/RoleWind.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace uchebka32.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для RolePage.xaml
+    /// </summary>
+    public partial class RolePage : Window
+    {
+        public RolePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
