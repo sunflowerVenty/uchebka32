187007b697cdb8816ba0c621593a289935cc0816
Юлия Карелова <j.karelowa@gmail.com>
2025-04-01 10:00:24 +0300

    Добавьте файлы проекта.

---
+++ uchebka32/Window/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using uchebka32.Pages;
+
+namespace uchebka32
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.NavigationService.Navigate(new MainPage());
+        }
+    }
+}
